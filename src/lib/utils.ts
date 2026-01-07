65ee087944944332698a5c303bc241f7a9228e31
SerikovZhalgas <zhalgasserikov@gmail.com>
2026-01-07 21:46:45 +0500

    updated: changed kk text;

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,7 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export function handleBooking(language: 'ru' | 'kk', isFranchise: boolean) {
   const phoneNumber = "77761259549";
-  const message = encodeURIComponent(language === 'ru' ? "Здравствуйте! Хочу записаться на курс." : "Сәлеметсіз бе! Курсқа жазылғым келеді.");
+  const message = encodeURIComponent(language === 'ru' ? "Здравствуйте! Хочу записаться на курс." : "Сәлеметсіз бе! Сабаққа жазылайын деп едім..");
   const messageFranchise = encodeURIComponent(language === 'ru' ? "Здравствуйте! Расскажите подробнее о франшизе." : "Сәлеметсіз бе! Франшиза туралы толығырақ айтып беріңізші.");
   const whatsappUrl = `https://wa.me/${phoneNumber}?text=${isFranchise ? messageFranchise : message}`;
   window.open(whatsappUrl, '_blank');
